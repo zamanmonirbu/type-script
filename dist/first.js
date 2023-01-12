d8da9a2417b4131116de1687a66abf8f052d169d
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2023-01-12 18:26:27 +0600

    first commit

---
+++ dist/first.js
@@ -0,0 +1,4 @@
+"use strict";
+let money = 45;
+let nameString = "A String";
+console.log(money, nameString);
---
+++ first.ts
@@ -0,0 +1,4 @@
+let money:number=45;
+let nameString:string="A String";
+console.log(money,nameString);
+
